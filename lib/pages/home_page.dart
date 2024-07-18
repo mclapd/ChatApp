191240e7a58621bb836ff52dfbe3c33f8a10d734
one_punch_man <ckim@banlaw.com>
2024-07-18 16:07:18 +1000

    chore: Add MyDrawer component and update HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,16 @@
-import 'package:chatapp/auth/auth_service.dart';
+import 'package:chatapp/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void logout() {
-    final auth = AuthService();
-    auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        actions: [
-          IconButton(onPressed: logout, icon: const Icon(Icons.logout))
-        ],
       ),
+      drawer: const MyDrawer(),
     );
   }
 }
